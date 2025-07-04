f77ce4ecc2584a6a038c5cb17d1efcaaec5952e5
alymbekovaAliya <120567530+alymbekovaAliya@users.noreply.github.com>
2025-07-04 10:38:25 +0600

    Update Draft2.java

--- src/main/java/Draft/Draft2.java
+++ src/main/java/Draft/Draft2.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class Draft2 {
     public static void main(String[] args) {
-//        List<Integer> numbers = Arrays.asList(-2, 0, 3, 4, -5, 6, 7);
+
 //
 //        List<Integer> numbers2 =  numbers.stream().filter(n -> n > 0 && n % 2 == 0).toList();
 //        System.out.println(numbers2);
