22d662aeaf51faa1c4437f440d9f33ff3d4d2141
Aliia <aalymbekova.aliya@icloud.com>
2025-07-07 11:33:50 +0600

    112

--- src/main/java/com/autobots/java/version.java
+++ src/main/java/com/autobots/java/version.java
@@ -3,5 +3,6 @@ package com.autobots.java;
 public class version {
     public static void main(String[] args) {
         System.out.println("version 111");
+        System.out.println("version 112git");
     }
 }
